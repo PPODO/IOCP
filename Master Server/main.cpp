801808bdb460a55b136d933e143c7ded4392825a
PPODO <sherlockwhooo@gmail.com>
2020-04-03 03:00:40 +0900

    update main.cpp

--- Master Server/main.cpp
+++ Master Server/main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 #include <Net/MemoryPool.hpp>
 
-class A : public MemoryManagement::CMemoryPool<A, 1> {
-public:
-	A(int a, float b, char c) : mA(a), mB(b), mC(c) {};
-
-public:
-	int mA;
-	float mB;
-	char mC;
-
-};
-
 int main() {
 
 
