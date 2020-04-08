4469881e90cd6830fe1ebc6c94d5cada895b1a03
PPODO <sherlockwhooo@gmail.com>
2020-04-08 23:26:30 +0900

    Create Config.h

---
+++ Net/Config.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <cstdio>
+#include <memory>
+#include <Windows.h>
+#include <exception>
+
+class CConfig {
+	static const size_t buffer_length = 32;
+	friend std::shared_ptr<const CConfig> LoadIni(const char* const filePath) noexcept;
+private:
+	CConfig() noexcept : 
+		mMaxThreadCount(), mMaxClientCount(),
+		mPortNumber(), mProtocolType() {
+		ZeroMemory(mIPAddress, buffer_length);
+	}
+
+public:
+	int mMaxThreadCount;
+	int mMaxClientCount;
+
+public:
+	char mIPAddress[buffer_length];
+	int mPortNumber;
+	int mProtocolType;
+
+};
+
+static std::shared_ptr<const CConfig> LoadIni(const char* const filePath) noexcept {
+	CConfig* Result = nullptr;
+	try {
+		Result = new CConfig;
+
+		Result->mMaxThreadCount = GetPrivateProfileInt("System", "Max Thread Count", 0, filePath);
+		Result->mMaxClientCount = GetPrivateProfileInt("System", "Max Client Count", 0, filePath);
+
+		GetPrivateProfileString("Networking", "IP Address", "0.0.0.0", Result->mIPAddress, CConfig::buffer_length, filePath);
+		Result->mPortNumber = GetPrivateProfileInt("Networking", "Port Number", 3550, filePath);
+		Result->mProtocolType = GetPrivateProfileInt("Networking", "Protocol Type", 0, filePath);
+
+	}
+	catch (const std::exception& Exception) {
+		printf("Exception - %s", Exception.what());
+	}
+	return std::shared_ptr<CConfig>(Result);
+}
