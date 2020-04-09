c689f3ef9315fd754fd326854e9820716d0ffc48
PPODO <sherlockwhooo@gmail.com>
2020-04-09 20:11:25 +0900

    Update Socket

--- Net/Socket.cpp
+++ Net/Socket.cpp
@@ -1,19 +1,21 @@
 #pragma comment(lib, "mswsock.lib")
 #include "Socket.h"
 #include "SocketAddress.h"
+#include "Iocp.h"
+#include "OverlappedType.h"
 #include <MSWSock.h>
 
 using namespace IPEndPoint;
 
 namespace TcpSocket {
-	CTcpSocket::CTcpSocket() {
+	CTcpSocket::CTcpSocket(size_t defaultBufferSize) : mReceiveBuffer(defaultBufferSize) {
 		mSock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, WSA_FLAG_OVERLAPPED);
 		if (mSock == INVALID_SOCKET) {
 			throw std::exception("bad initialization!");
 		}
 	}
 
-	CTcpSocket::CTcpSocket(SOCKET hSocket) noexcept : mSock(hSocket) {}
+	CTcpSocket::CTcpSocket(SOCKET hSocket, size_t defaultBufferSize) noexcept : mReceiveBuffer(defaultBufferSize), mSock(hSocket) {}
 
 	CTcpSocket::~CTcpSocket() {
 		closesocket(mSock);
@@ -44,12 +46,52 @@ namespace TcpSocket {
 	}
 
 	bool CTcpSocket::Accept(const CTcpSocket& listenSocket) noexcept {
-		/*if (AcceptEx(listenSocket.mSock, mSock, , 0, CIPEndPoint::GetSize() + 16, CIPEndPoint::GetSize() + 16, , ) == SOCKET_ERROR) {
-			if (WSAGetLastError() != WSA_IO_PENDING && WSAGetLastError() != WSAEWOULDBLOCK) {
+		auto acceptOverlapped = new Overlapped::Accept_Overlapped(this);
 
+		if (AcceptEx(listenSocket.mSock, mSock, &acceptOverlapped->mOutBuffer, 0, CIPEndPoint::GetSize() + 16, CIPEndPoint::GetSize() + 16, &acceptOverlapped->mUsedBytes, &acceptOverlapped->mOverlapped) == SOCKET_ERROR) {
+			if (WSAGetLastError() != WSA_IO_PENDING && WSAGetLastError() != WSAEWOULDBLOCK) {
+				Overlapped::DeleteIOContext(acceptOverlapped);
+				std::cout << "Error\n";
 				return false;
 			}
-		}*/
+		}
+		return true;
+	}
+
+	bool CTcpSocket::PreReceive() {
+		auto receiveZeroOverlapped = new Overlapped::ReceiveZero_Overlapped(nullptr, 0);
+		if (WSARecv(mSock, &receiveZeroOverlapped->mWSABuf, 1, &receiveZeroOverlapped->mUsedBytes, &receiveZeroOverlapped->mFlag, &receiveZeroOverlapped->mOverlapped, NULL) == SOCKET_ERROR) {
+			if (WSAGetLastError() != WSA_IO_PENDING && WSAGetLastError() != WSAEWOULDBLOCK) {
+				Overlapped::DeleteIOContext(receiveZeroOverlapped);
+				std::cout << "Error! - " << WSAGetLastError() << std::endl;
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool CTcpSocket::PostReceive() {
+		char Buffer[3];
+		auto receiveOverlapped = new Overlapped::Receive_Overlapped(Buffer, 2);
+
+		if (WSARecv(mSock, &receiveOverlapped->mWSABuf, 1, &receiveOverlapped->mUsedBytes, &receiveOverlapped->mFlag, &receiveOverlapped->mOverlapped, NULL) == SOCKET_ERROR) {
+			if (WSAGetLastError() != WSA_IO_PENDING && WSAGetLastError() != WSAEWOULDBLOCK) {
+				Overlapped::DeleteIOContext(receiveOverlapped);
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool CTcpSocket::Send(const char* const buffer) {
+		auto sendOverlapped = new Overlapped::Send_Overlapped(buffer, strlen(buffer));
+
+		if (WSASend(mSock, &sendOverlapped->mWSABuf, 1, &sendOverlapped->mUsedBytes, 0, &sendOverlapped->mOverlapped, NULL) == SOCKET_ERROR) {
+			if (WSAGetLastError() != WSA_IO_PENDING && WSAGetLastError() != WSAEWOULDBLOCK) {
+				Overlapped::DeleteIOContext(sendOverlapped);
+				return false;
+			}
+		}
 		return true;
 	}
 
--- Net/Socket.h
+++ Net/Socket.h
@@ -1,7 +1,8 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma once
-#pragma(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")
 #include <WinSock2.h>
+#include "CircularBuffer.hpp"
 
 namespace IPEndPoint {
 	class CIPEndPoint;
@@ -10,8 +11,8 @@ namespace IPEndPoint {
 namespace TcpSocket {
 	class CTcpSocket {
 	public:
-		explicit CTcpSocket();
-		explicit CTcpSocket(SOCKET hSocket) noexcept;
+		explicit CTcpSocket(size_t defaultBufferSize);
+		explicit CTcpSocket(SOCKET hSocket, size_t defaultBufferSize) noexcept;
 		~CTcpSocket() noexcept;
 
 	public:
@@ -19,12 +20,16 @@ namespace TcpSocket {
 		bool Listen(int backLog) noexcept;
 		bool Connect(const class IPEndPoint::CIPEndPoint& connectAddress) noexcept;
 		bool Accept(const CTcpSocket& listenSocket) noexcept;
+		bool PreReceive();
+		bool PostReceive();
+		bool Send(const char* const buffer);
 
 	public:
-
+		__forceinline SOCKET GetSocketHandle() const { return mSock; }
 
 	private:
 		SOCKET mSock;
+		Buffer::CCircularBuffer mReceiveBuffer;
 
 	};
 
