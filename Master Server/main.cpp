0546ede6bbd8ad88ac6d8112913aded1aa07dbc6
PPODO <sherlockwhooo@gmail.com>
2020-04-09 20:12:46 +0900

    Create main.cpp

--- Master Server/main.cpp
+++ Master Server/main.cpp
@@ -1,61 +1,11 @@
 #include <iostream>
-
-/*template<size_t buffer_size = 512>
-class CCircularBuffer {
-public:
-	CCircularBuffer() : mBufferEnd(mBuffer + (buffer_size - 1)), mBufferFront(mBuffer), mBufferRear(mBuffer) {
-		std::memset(mBuffer, 0, buffer_size * sizeof(char));
-	}
-
-public:
-	void Push(const char* const data, size_t length) {
-		if (length > buffer_size - 1) {
-			std::cout << "length can be greater than buffer_size!";
-			return;
-		}
-
-		if (length <= mBufferEnd - mBufferRear - 1) {
-			std::memcpy(mBufferRear, data, length);
-			mBufferRear += length;
-		}
-		else {
-			int remainBytes = mBufferEnd - mBufferRear;
-			std::memcpy(mBufferRear, data, remainBytes);
-			mBufferRear = mBuffer;
-			std::memcpy(mBufferRear + remainBytes, data, length - remainBytes);
-			mBufferRear += (length - remainBytes);
-		}
-	}
-
-	void Pop(char* const buffer, size_t length) {
-		if (length > buffer_size - 1) {
-			std::cout << "length can be less than buffer_size!";
-			return;
-		}
-
-		if (length <= mBufferEnd - mBufferFront - 1) {
-			std::memcpy(buffer, mBufferFront, length);
-			mBufferFront += length;
-		}
-		else {
-			int remainBytes = mBufferEnd - mBufferFront;
-			std::memcpy(buffer, mBufferFront, remainBytes);
-			mBufferFront = mBuffer;
-			std::memcpy(buffer, mBufferFront, length - remainBytes);
-			mBufferFront += length - remainBytes;
-		}
-	}
-
-
-private:
-	char mBuffer[buffer_size];
-	char* const mBufferEnd;
-	char* mBufferFront;
-	char* mBufferRear;
-
-};*/
+#include <Net/Iocp.h>
 
 int main() {
+	IOCP::CIOCP iocp(LoadIni("../test.ini"));
+
+	iocp.Initialize();
+	iocp.Run();
 
 	return 0;
 }
---
+++ test_client/main.cpp
@@ -0,0 +1,18 @@
+#include <Net/Socket.h>
+#include <Net/SocketAddress.h>
+
+int main() {
+	WSADATA WinSockData;
+	WSAStartup(WINSOCK_VERSION, &WinSockData);
+
+	TcpSocket::CTcpSocket Sock(1024);
+
+	Sock.Connect(IPEndPoint::CIPEndPoint("127.0.0.1", 3550));
+	Sleep(1000);
+
+	send(Sock.GetSocketHandle(), "HIHI", 4, 0);
+
+	while (true);
+	WSACleanup();
+	return 0;
+}
