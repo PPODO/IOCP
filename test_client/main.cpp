df8675c925631dc5b06b9c3719817c26b772d666
PPODO <sherlockwhooo@gmail.com>
2020-04-18 23:03:19 +0900

    Update main

--- test_client/main.cpp
+++ test_client/main.cpp
@@ -5,12 +5,12 @@ int main() {
 	WSADATA WinSockData;
 	WSAStartup(WINSOCK_VERSION, &WinSockData);
 
-	TcpSocket::CTcpSocket Sock(1024);
+	TcpSocket::CTcpSocket Sock(1024, 0);
 
 	Sock.Connect(IPEndPoint::CIPEndPoint("127.0.0.1", 3550));
 	Sleep(1000);
 
-	send(Sock.GetSocketHandle(), "HIHI", 4, 0);
+	send(Sock.GetSocketHandle(), "HINO", 4, 0);
 
 	while (true);
 	WSACleanup();
