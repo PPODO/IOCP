e1bb1218057eb2b04d7aff2be38af7d2c64115ba
unknown <sherlockwhooo@gmail.com>
2020-08-01 02:39:49 +0900

    update

--- BattleRacing-Database/mysql_header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "mysql_pool.h"
-#include "mysql_manager.h"
-#include "mysql_connect.h"
-
-class mysql_data {
-public:
-	virtual void SetData(const std::vector<char*>& datas) = 0;
-};
--- BattleRacing-Server/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "mswsock.lib");
-#include <iostream>
-#include <WinSock2.h>
-#include <MSWSock.h>
